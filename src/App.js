2b7d409cfcd5ed5eb1b14d2a4f4fafadc8f01cf8
GameChanger77 <gamer4christ2004@gmail.com>
2023-11-12 14:47:47 -0600

    Got the data retrieval in Tracker.js to work.

--- src/App.js
+++ src/App.js
@@ -13,22 +13,27 @@ function App() {
     switch (page) {
       case 0: // Tracker Page
         return (<Tracker />)
-        break;
       case 1: // Settings Page
         return (<Settings />)
-        break;
       case 2: // About Page
         return (<About />)
-        break;
     }
   }
 
 
   return (
     <div>
+      <header>
+        This is a header that is in all of the veiws but is only programmed in the app.js
+        <h1>Realtime Marine Tracker</h1>
+        <button id="about-button" class="about-button">About</button>
+      </header>
       
       {displayPage()}
     
+    <footer>
+      This is a footer that is in all of the veiws but is only programmed in the app.js
+    </footer>
     </div>
   );
 }
--- src/Tracker.js
+++ src/Tracker.js
@@ -1,8 +1,43 @@
-
+import React, { useState, useEffect } from 'react';
 
 const Tracker = () => {
+  const [data, setData] = useState([]);
+  const [temp, setTemp] = useState(-999);
+  const [humidity, setHumidity] = useState(-1);
+  const [waterLevel, setWaterLevel] = useState(-1);
 
-    return (<div>this is the tracker page</div>);
-}
+  function loadData(data) {
+    console.log(data);
+    setData(data);
+    setTemp(data.temp);
+    setHumidity(data.humidity);
+    setWaterLevel(data.waterLevel);
+  }
+
+  function fetchData() {
+    fetch('./data.json')
+      .then((response) => response.json())
+      .then((data) => loadData(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  }
+
+  // Use useEffect to fetch data on component mount
+  useEffect(() => {
+    fetchData();
+
+    // Set up interval to fetch data every 2000 milliseconds
+    const intervalId = setInterval(fetchData, 2000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures useEffect runs only once on mount
+
+  return (
+    <div>
+      this is the tracker page
+      <div>{humidity}</div>
+    </div>
+  );
+};
 
 export default Tracker;
